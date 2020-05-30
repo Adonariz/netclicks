e4d4d51f9fe104e45fc2041a0b9afd2790dafc3c
A Zhila <aleksej.zhila@gmail.com>
2020-05-30 20:51:51 +0300

    Добавляет закрытие бокового меню при клике вне меню

--- js/main.js
+++ js/main.js
@@ -7,3 +7,10 @@ burger.addEventListener(`click`, () => {
   leftMenu.classList.toggle(`openMenu`);
   burger.classList.toggle(`open`);
 });
+
+document.body.addEventListener(`click`, (evt) => {
+  if (!evt.target.closest(`.left-menu`)) {
+    leftMenu.classList.remove(`openMenu`);
+    burger.classList.remove(`open`);
+  }
+});
